7495afe950fd9c83bdd3ff1c7c3f6e628d1800ab
wolfowlshield <btouch12@gmail.com>
2021-09-29 09:42:52 -0700

    Added a ton of notes explaining Testop to newcomers

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testop.java
@@ -5,24 +5,35 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
+// This is an opmode that tests to see if we have successfully gotten code to the robot
+//  It also goes through and explains everything that goes on
 
-@Autonomous(name = "Autonomous")
-public class Testop extends LinearOpMode {
 
-    private DcMotor testMotor;
+@Autonomous(name = "Autonomous") // The string here is what the OpMode Shows up as on the phone
+                                 // and the @Autonomous declares this OpMode as an Autonomous one
 
-    @Override
-    public void runOpMode() throws InterruptedException {
+public class Testop extends LinearOpMode { // LinearOpMode is the base of all the OpModes we will be using
 
-        testMotor = hardwareMap.get(DcMotor.class, "testMotor");
+    private DcMotor testMotor; // Creates a DcMotor Object
 
-        testMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+    @Override // This is overriding a previously defined method. I.E. runOpMode() already exists,
+              // but we override it, and thats where everything physical happens
 
-        waitForStart();
-        
-        testMotor.setPower(0.5);
-        sleep(1000);
-        testMotor.setPower(0);
+    public void runOpMode() throws InterruptedException { // "throws InterruptedException" is saying that when something goes
+                                                          // terribly wrong, it will stop the code.
+                                                          // DO NOT REMOVE: it could lead to serious damage to our robot
 
+        testMotor = hardwareMap.get(DcMotor.class, "testMotor"); // The deviceName MUST match the name of
+                                                                           // The motor on the phone
+
+        testMotor.setDirection(DcMotorSimple.Direction.FORWARD); // Might not be necessary,
+                                                                 // but all of last years code did something similar
+
+        waitForStart(); // Make sure to waitForStart() before turning on any motors/servos...
+                        // If you don't it will get angry, and throw an Interrupted Exception
+
+        testMotor.setPower(0.5); // Turn on at half power
+        sleep(1000);   // Wait 1 second
+        testMotor.setPower(0);   // Turn off
     }
 }
